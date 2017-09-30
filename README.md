9ecbb2b6ac1ebff8cfb7612c894fde3580585af6
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-09-30 19:36:04 -0400

    Add model and register

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Authentican App
 
-## This app was created following tutorial series https://www.youtube.com/watch?v=uONz0lEWft0. It's made using MEAN stack front to back.
+#### This app was created following tutorial series https://www.youtube.com/watch?v=uONz0lEWft0. It's made using MEAN stack front to back.
 
-### This update sets up the backend Express server with dependencies from Mongoose, BCryptjs, Passport, etc. We also specify routes such as /register, /profile, etc.
+##### So far...
+- set up the backend Express server with dependencies from Mongoose, BCryptjs, Passport, etc. 
+- specified routes such as /register, /profile, etc.
+- completed /register post to sign up a new user, and encrypt passwords with bcrypt
 
-#### Note:
+###### Note:
 node_modules folder not included. Please run `npm install` to create it.
 
-#### To run this build, execute `npm start` or `nodemon`.
+###### To run this build, execute `npm start` or `nodemon`. Make sure to have MongoDB running in the background.
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
 //Register
-router.get('/register', (req, res, next) => {
-    res.send('Register');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password 
+    });
+
+    User.addUser(newUser, function(error, user) {
+        if (error) {
+            res.json({success: false, message: 'Failed to register user'});
+        } else {
+            res.json({success: true, message: 'User registered!'});
+        }
+    })
 });
 
 //Authenticate
@@ -14,11 +32,6 @@ router.post('/authenticate', (req, res, next) => {
 //profile
 router.get('/profile', (req, res, next) => {
     res.send('Profile');
-});
-
-//validate
-router.get('/validate', (req, res, next) => {
-    res.send('Validate');
 });
 
 module.exports = router;
